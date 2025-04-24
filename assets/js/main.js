5c3200ceec1df859fd94d724858ed4217b234ec6
frontenddev <frontendev344@gmail.com>
2025-04-24 10:53:45 +0530

    fixd

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,18 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
   });
+
+
+  // Read More Functionality JS Start
+$('.subpage-readmore-btn').click(function (e) {
+    e.preventDefault();
+    const $this = $(this);
+    const content = $this.parent().prev();
+    content.stop(true, true).slideToggle(500);
+    $this.html(function (_, currentHtml) {
+      return currentHtml.includes('Read More')
+        ? 'Read Less <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M201.4 137.4c12.5-12.5 32.8-12.5 45.3 0l160 160c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 205.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160z"/></svg>'
+        : 'Read More <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg>';
+    });
+  });
+  // Read More Functionality JS end
