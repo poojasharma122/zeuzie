b349359015f786f939c86e0f6ed26b17fb3ec697
frontenddev <frontendev344@gmail.com>
2025-04-30 17:33:50 +0530

    dsg

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,13 +23,16 @@ document.addEventListener("DOMContentLoaded", function () {
   // Read More Functionality JS Start
   $('.subpage-readmore-btn').click(function (e) {
     e.preventDefault();
-    const $this = $(this);
-    const content = $this.parent().prev();
-    content.stop(true, true).slideToggle(500);
-    $this.html(function (_, currentHtml) {
-      return currentHtml.includes('Διαβάστε Περισσότερα')
-        ? 'Διαβάστε Λιγότερα <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M201.4 137.4c12.5-12.5 32.8-12.5 45.3 0l160 160c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 205.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160z"/></svg>'
-        : 'Διαβάστε Περισσότερα <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg>';
+    const $button = $(this);
+    const $content = $button.closest('.blogpost-text').find('.blogpost-content');
+
+    $content.stop(true, true).slideToggle(500, function () {
+        const isVisible = $content.is(':visible');
+
+        $button.html(isVisible
+            ? 'Διαβάστε Λιγότερα <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M201.4 137.4c12.5-12.5 32.8-12.5 45.3 0l160 160c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 205.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160z"/></svg>' // UP arrow
+            : 'Διαβάστε Περισσότερα <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg>' // DOWN arrow
+        );
     });
 });
 
